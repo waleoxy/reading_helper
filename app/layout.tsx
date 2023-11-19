5744a5b02d0282155bd5074acd29d3baf4f5b06c
Olawale Otubu <waleoxy@gmail.com>
2023-11-19 17:17:04 +0100

    feat:dashboard detail

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "../lib/utils";
 import Navbar from "./_components/Navbar";
 import Providers from "../components/providers";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
