1193018622d614948931a0c4c3584dad3e64362e
Olawale Otubu <waleoxy@gmail.com>
2023-12-14 10:50:27 +0100

    fix:kinde setupv10

--- app/(pages)/pricing/page.tsx
+++ app/(pages)/pricing/page.tsx
@@ -38,6 +38,7 @@ const PricingPage: React.FC<PricingPageProps> = async ({}) => {
         },
         {
           text: "Mobile-friendly interface",
+          negative: false,
         },
         {
           text: "Higher-quality responses",
@@ -58,20 +59,25 @@ const PricingPage: React.FC<PricingPageProps> = async ({}) => {
         {
           text: "25 pages per PDF",
           footnote: "The maximum amount of pages per PDF-file.",
+          negative: false,
         },
         {
           text: "16MB file size limit",
           footnote: "The maximum file size of a single PDF file.",
+          negative: false,
         },
         {
           text: "Mobile-friendly interface",
+          negative: false,
         },
         {
           text: "Higher-quality responses",
           footnote: "Better algorithmic responses for enhanced content quality",
+          negative: false,
         },
         {
           text: "Priority support",
+          negative: false,
         },
       ],
     },
