ae8ccd81fd253769cdccbea1627fdd823e9a9e97
Olawale Otubu <waleoxy@gmail.com>
2023-12-14 10:39:18 +0100

    fix:kinde setupv7

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 }
